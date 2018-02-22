d597765865d344944a1d822c4f92ed8bde4a2fd6
Roman Storm <rstormsf@gmail.com>
2018-02-21 21:52:11 -0800

    WIP add Validators

--- contracts/ForeignBridge.sol
+++ contracts/ForeignBridge.sol
@@ -2,113 +2,19 @@ pragma solidity 0.4.19;
 import "./libraries/Helpers.sol";
 import "./libraries/Message.sol";
 import "./libraries/MessageSigning.sol";
-// import "./libraries/SafeMath.sol";
-import "./IBurnableMintableERC827Token.sol";
-import "./POA20.sol";
+import "./BridgeValidators.sol";
 
 contract ForeignBridge {
-    // following is the part of ForeignBridge that implements an ERC20 token.
-    // ERC20 spec: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
-
-    // uint public totalSupply;
-
-    // string public name = "POA ERC20 Foundation";
-    // string public symbol = "POA20";
-    // /// maps addresses to their token balances
-    // mapping (address => uint) public balances;
-
-    // // owner of account approves the transfer of an amount by another account
-    // mapping(address => mapping (address => uint)) allowed;
-
-    // /// Event created on money transfer
-    // event Transfer(address indexed from, address indexed to, uint tokens);
-
-    // // returns the ERC20 token balance of the given address
-    // function balanceOf(address tokenOwner) public view returns (uint) {
-    //     return balances[tokenOwner];
-    // }
-
-    // /// Transfer `value` to `recipient` on this `foreign` chain.
-    // ///
-    // /// does not affect `home` chain. does not do a relay.
-    // /// as specificed in ERC20 this doesn't fail if tokens == 0.
-    // function transfer(address recipient, uint tokens) public returns (bool) {
-    //     require(balances[msg.sender] >= tokens);
-    //     // fails if there is an overflow
-    //     require(balances[recipient] + tokens >= balances[recipient]);
-
-    //     balances[msg.sender] -= tokens;
-    //     balances[recipient] += tokens;
-    //     Transfer(msg.sender, recipient, tokens);
-    //     return true;
-    // }
-
-    // // following is the part of ForeignBridge that is concerned
-    // // with the part of the ERC20 standard responsible for giving others spending rights
-    // // and spending others tokens
-
-    // // created when `approve` is executed to mark that
-    // // `tokenOwner` has approved `spender` to spend `tokens` of his tokens
-    // event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-
-    // // allow `spender` to withdraw from your account, multiple times, up to the `tokens` amount.
-    // // calling this function repeatedly overwrites the current allowance.
-    // function approve(address spender, uint tokens) public returns (bool) {
-    //     allowed[msg.sender][spender] = tokens;
-    //     Approval(msg.sender, spender, tokens);
-    //     return true;
-    // }
-
-    // // returns how much `spender` is allowed to spend of `owner`s tokens
-    // function allowance(address owner, address spender) public view returns (uint256) {
-    //     return allowed[owner][spender];
-    // }
-
-    // function transferFrom(address from, address to, uint tokens) public returns (bool) {
-    //     // `from` has enough tokens
-    //     require(balances[from] >= tokens);
-    //     // `sender` is allowed to move `tokens` from `from`
-    //     require(allowed[from][msg.sender] >= tokens);
-    //     // fails if there is an overflow
-    //     require(balances[to] + tokens >= balances[to]);
-
-    //     balances[to] += tokens;
-    //     balances[from] -= tokens;
-    //     allowed[from][msg.sender] -= tokens;
-
-    //     Transfer(from, to, tokens);
-    //     return true;
-    // }
-
-    // following is the part of ForeignBridge that is
-    // no longer part of ERC20 and is concerned with
-    // with moving tokens from and to HomeBridge
-
-    struct SignaturesCollection {
-        /// Signed message.
-        bytes message;
-        /// Authorities who signed the message.
-        address[] signed;
-        /// Signatures
-        bytes[] signatures;
-    }
-
-    /// Number of authorities signatures required to withdraw the money.
-    ///
-    /// Must be less than number of authorities.
-    uint public requiredSignatures;
-
-    /// Contract authorities.
-    address[] public authorities;
-
-    /// Pending deposits and authorities who confirmed them
-    mapping (bytes32 => address[]) deposits;
+    mapping (bytes32 => bool) deposits_signed;
+    mapping (bytes32 => uint8) num_deposits_signed;
+	mapping (bytes32 => bool) mintRequestsDone;
+	mapping (bytes32 => uint8) withdrawRequests;
 
     /// Pending signatures and authorities who confirmed them
-    mapping (bytes32 => SignaturesCollection) signatures;
+    BridgeValidators public validatorsContract;
 
     /// Token contract which the bridge has full ownership to burn and mint
-    POA20 public token;
+    address public token;
 
     /// triggered when relay of deposit from HomeBridge is complete
     event Deposit(address recipient, uint value);
@@ -120,41 +26,35 @@ contract ForeignBridge {
     event CollectedSignatures(address authorityResponsibleForRelay, bytes32 messageHash);
 
     function ForeignBridge(
-        uint _requiredSignatures,
-        address[] _authorities, 
-        address _token
+        address _token,
+        address _validatorsContract
     ) public
     {
-        require(_requiredSignatures != 0);
-        require(_requiredSignatures <= _authorities.length);
-        require(_token != address(0));
-        requiredSignatures = _requiredSignatures;
-        authorities = _authorities;
-        token = POA20(_token);
+        require(_token != address(0) && _validatorsContract != address(0));
+        validatorsContract = BridgeValidators(_validatorsContract);
+        token = _token;
     }
 
-    /// require that sender is an authority
-    modifier onlyAuthority() {
-        require(Helpers.addressArrayContains(authorities, msg.sender));
-        _;
-    }
 
     /// Used to deposit money to the contract.
     ///
     /// deposit recipient (bytes20)
     /// deposit value (uint)
     /// mainnet transaction hash (bytes32) // to avoid transaction duplication
-    function deposit(address recipient, uint value, bytes32 transactionHash) public onlyAuthority() {
-        // Protection from misbehaving authority
-        var hash = keccak256(recipient, value, transactionHash);
+    function deposit(address recipient, uint256 value, bytes32 transactionHash) public {
+        require(validatorsContract.onlyValidator(msg.sender));
+        bytes32 hash_msg = keccak256(recipient, value, transactionHash);
+        bytes32 hash_sender = keccak256(msg.sender, hash_msg);
 
-        // don't allow authority to confirm deposit twice
-        require(!Helpers.addressArrayContains(deposits[hash], msg.sender));
+        // Prevents Duplicated deposits
+        require(!deposits_signed[hash_sender]);
+        deposits_signed[hash_sender] = true;
 
-        deposits[hash].push(msg.sender);
-        // TODO: this may cause troubles if requiredSignatures len is changed
-        if (deposits[hash].length == requiredSignatures) {
-            token.mint(recipient, value);
+        uint8 signed = num_deposits_signed[hash_msg] + 1;
+        num_deposits_signed[hash_msg] = signed;
+
+        if (signed == requiredValidators) {
+            // token.mint(recipient, value);
             Deposit(recipient, value);
         }
     }
@@ -180,7 +80,8 @@ contract ForeignBridge {
     /// withdrawal recipient (bytes20)
     /// withdrawal value (uint)
     /// foreign transaction hash (bytes32) // to avoid transaction duplication
-    function submitSignature(bytes signature, bytes message) public onlyAuthority() {
+    function submitSignature(bytes signature, bytes message) public {
+        require(validatorsContract.onlyValidator(msg.sender));
         // Validate submited signatures
         require(MessageSigning.recoverAddressFromSignedMessage(signature, message) == msg.sender);
 
@@ -189,24 +90,24 @@ contract ForeignBridge {
         var hash = keccak256(message);
 
         // Duplicated signatures
-        require(!Helpers.addressArrayContains(signatures[hash].signed, msg.sender));
-        signatures[hash].message = message;
-        signatures[hash].signed.push(msg.sender);
-        signatures[hash].signatures.push(signature);
+        // require(!Helpers.addressArrayContains(signatures[hash].signed, msg.sender));
+        // signatures[hash].message = message;
+        // signatures[hash].signed.push(msg.sender);
+        // signatures[hash].signatures.push(signature);
 
         // TODO: this may cause troubles if requiredSignatures len is changed
-        if (signatures[hash].signed.length == requiredSignatures) {
+        // if (signatures[hash].signed.length == requiredSignatures) {
             CollectedSignatures(msg.sender, hash);
-        }
+        // }
     }
 
     /// Get signature
-    function signature(bytes32 hash, uint index) public view returns (bytes) {
-        return signatures[hash].signatures[index];
-    }
+    // function signature(bytes32 hash, uint index) public view returns (bytes) {
+    //     return signatures[hash].signatures[index];
+    // }
 
-    /// Get message
-    function message(bytes32 hash) public view returns (bytes) {
-        return signatures[hash].message;
-    }
+    // /// Get message
+    // function message(bytes32 hash) public view returns (bytes) {
+    //     return signatures[hash].message;
+    // }
 }

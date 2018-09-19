3c48744826a79d360679a2df00610df7454bf995
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2018-09-19 15:00:25 -0300

    Add home erc20-to-native tests

---
+++ test/erc_to_native/home_bridge.test.js
@@ -0,0 +1,209 @@
+const Web3Utils = require('web3-utils')
+const HomeBridge = artifacts.require('HomeBridgeErcToNative.sol')
+const EternalStorageProxy = artifacts.require('EternalStorageProxy.sol')
+const BridgeValidators = artifacts.require('BridgeValidators.sol')
+const BlockReward = artifacts.require('BlockReward')
+const {ERROR_MSG, ZERO_ADDRESS} = require('../setup');
+const minPerTx = web3.toBigNumber(web3.toWei(0.01, "ether"));
+const requireBlockConfirmations = 8;
+const gasPrice = Web3Utils.toWei('1', 'gwei');
+const oneEther = web3.toBigNumber(web3.toWei(1, "ether"));
+const halfEther = web3.toBigNumber(web3.toWei(0.5, "ether"));
+
+contract('HomeBridge_ERC20_to_Native', async (accounts) => {
+  let homeContract, validatorContract, blockRewardContract, authorities, owner;
+  before(async () => {
+    validatorContract = await BridgeValidators.new()
+    blockRewardContract = await BlockReward.new()
+    authorities = [accounts[1]]
+    owner = accounts[0]
+    await validatorContract.initialize(1, authorities, owner)
+  })
+
+  describe('initialize', async() => {
+    beforeEach(async () => {
+      homeContract = await HomeBridge.new()
+    })
+    it('sets variables', async () => {
+      ZERO_ADDRESS.should.be.equal(await homeContract.validatorContract())
+      '0'.should.be.bignumber.equal(await homeContract.deployedAtBlock())
+      '0'.should.be.bignumber.equal(await homeContract.dailyLimit())
+      '0'.should.be.bignumber.equal(await homeContract.maxPerTx())
+      false.should.be.equal(await homeContract.isInitialized())
+      ZERO_ADDRESS.should.be.equal(await homeContract.blockRewardContract())
+
+      await homeContract.initialize(validatorContract.address, '3', '2', '1', gasPrice, requireBlockConfirmations, blockRewardContract.address).should.be.fulfilled
+
+      true.should.be.equal(await homeContract.isInitialized())
+      validatorContract.address.should.be.equal(await homeContract.validatorContract())
+      blockRewardContract.address.should.be.equal(await homeContract.blockRewardContract());
+      (await homeContract.deployedAtBlock()).should.be.bignumber.above(0)
+      '3'.should.be.bignumber.equal(await homeContract.dailyLimit())
+      '2'.should.be.bignumber.equal(await homeContract.maxPerTx())
+      '1'.should.be.bignumber.equal(await homeContract.minPerTx())
+    })
+    it('cant set maxPerTx > dailyLimit', async () => {
+      false.should.be.equal(await homeContract.isInitialized())
+
+      await homeContract.initialize(validatorContract.address, '1', '2', '1', gasPrice, requireBlockConfirmations, blockRewardContract.address).should.be.rejectedWith(ERROR_MSG)
+      await homeContract.initialize(validatorContract.address, '3', '2', '2', gasPrice, requireBlockConfirmations, blockRewardContract.address).should.be.rejectedWith(ERROR_MSG)
+
+      false.should.be.equal(await homeContract.isInitialized())
+    })
+
+    it('can be deployed via upgradeToAndCall', async () => {
+      let storageProxy = await EternalStorageProxy.new().should.be.fulfilled;
+      let data = homeContract.initialize.request(validatorContract.address, "3", "2", "1", gasPrice, requireBlockConfirmations, blockRewardContract.address).params[0].data
+
+      await storageProxy.upgradeToAndCall('1', homeContract.address, data).should.be.fulfilled
+      let finalContract = await HomeBridge.at(storageProxy.address);
+
+      true.should.be.equal(await finalContract.isInitialized())
+      validatorContract.address.should.be.equal(await finalContract.validatorContract())
+      blockRewardContract.address.should.be.equal(await finalContract.blockRewardContract())
+      "3".should.be.bignumber.equal(await finalContract.dailyLimit())
+      "2".should.be.bignumber.equal(await finalContract.maxPerTx())
+      "1".should.be.bignumber.equal(await finalContract.minPerTx())
+    })
+  })
+
+  describe('fallback', async () => {
+    beforeEach(async () => {
+      homeContract = await HomeBridge.new()
+      await homeContract.initialize(validatorContract.address, '3', '2', '1', gasPrice, requireBlockConfirmations, blockRewardContract.address)
+    })
+
+    it('should accept native coins', async () => {
+      const currentDay = await homeContract.getCurrentDay()
+      '0'.should.be.bignumber.equal(await homeContract.totalSpentPerDay(currentDay))
+
+      const {logs} = await homeContract.sendTransaction({ from: accounts[1], value: 1 }).should.be.fulfilled
+
+      logs[0].event.should.be.equal('UserRequestForSignature')
+      logs[0].args.should.be.deep.equal({ recipient: accounts[1], value: new web3.BigNumber(1) })
+      '1'.should.be.bignumber.equal(await homeContract.totalSpentPerDay(currentDay))
+      '1'.should.be.bignumber.equal(await homeContract.totalBurntCoins())
+    })
+
+    it('doesnt let you send more than daily limit', async () => {
+      const currentDay = await homeContract.getCurrentDay()
+      '0'.should.be.bignumber.equal(await homeContract.totalSpentPerDay(currentDay))
+
+      await homeContract.sendTransaction({ from: accounts[1], value: 1 }).should.be.fulfilled
+
+      '1'.should.be.bignumber.equal(await homeContract.totalSpentPerDay(currentDay))
+      '1'.should.be.bignumber.equal(await homeContract.totalBurntCoins())
+
+      await homeContract.sendTransaction({ from: accounts[1], value: 1 }).should.be.fulfilled;
+      '2'.should.be.bignumber.equal(await homeContract.totalSpentPerDay(currentDay))
+
+      await homeContract.sendTransaction({ from: accounts[1], value: 2 }).should.be.rejectedWith(ERROR_MSG);
+
+      await homeContract.setDailyLimit(4).should.be.fulfilled;
+      await homeContract.sendTransaction({ from: accounts[1], value: 2 }).should.be.fulfilled;
+      '4'.should.be.bignumber.equal(await homeContract.totalSpentPerDay(currentDay))
+      '4'.should.be.bignumber.equal(await homeContract.totalBurntCoins())
+    })
+
+    it('doesnt let you send more than max amount per tx', async () => {
+      await homeContract.sendTransaction({
+        from: accounts[1],
+        value: 1
+      }).should.be.fulfilled
+      await homeContract.sendTransaction({
+        from: accounts[1],
+        value: 3
+      }).should.be.rejectedWith(ERROR_MSG)
+      await homeContract.setMaxPerTx(100).should.be.rejectedWith(ERROR_MSG);
+      await homeContract.setDailyLimit(100).should.be.fulfilled;
+      await homeContract.setMaxPerTx(99).should.be.fulfilled;
+      //meets max per tx and daily limit
+      await homeContract.sendTransaction({
+        from: accounts[1],
+        value: 99
+      }).should.be.fulfilled
+      //above daily limit
+      await homeContract.sendTransaction({
+        from: accounts[1],
+        value: 1
+      }).should.be.rejectedWith(ERROR_MSG)
+
+    })
+
+    it('should not let to deposit less than minPerTx', async () => {
+      const newDailyLimit = 100;
+      const newMaxPerTx = 50;
+      const newMinPerTx = 20;
+
+      await homeContract.setDailyLimit(newDailyLimit).should.be.fulfilled;
+      await homeContract.setMaxPerTx(newMaxPerTx).should.be.fulfilled;
+      await homeContract.setMinPerTx(newMinPerTx).should.be.fulfilled;
+
+      await homeContract.sendTransaction({ from: accounts[1], value: newMinPerTx }).should.be.fulfilled
+      await homeContract.sendTransaction({ from: accounts[1], value: newMinPerTx - 1 }).should.be.rejectedWith(ERROR_MSG)
+    })
+  })
+
+  describe('setting limits', async () => {
+    let homeContract;
+    beforeEach(async () => {
+      homeContract = await HomeBridge.new()
+      await homeContract.initialize(validatorContract.address, '3', '2', '1', gasPrice, requireBlockConfirmations, blockRewardContract.address)
+    })
+    it('setMaxPerTx allows to set only to owner and cannot be more than daily limit', async () => {
+      await homeContract.setMaxPerTx(2, {from: authorities[0]}).should.be.rejectedWith(ERROR_MSG);
+      await homeContract.setMaxPerTx(2, {from: owner}).should.be.fulfilled;
+
+      await homeContract.setMaxPerTx(3, {from: owner}).should.be.rejectedWith(ERROR_MSG);
+    })
+
+    it('setMinPerTx allows to set only to owner and cannot be more than daily limit and should be less than maxPerTx', async () => {
+      await homeContract.setMinPerTx(1, {from: authorities[0]}).should.be.rejectedWith(ERROR_MSG);
+      await homeContract.setMinPerTx(1, {from: owner}).should.be.fulfilled;
+
+      await homeContract.setMinPerTx(2, {from: owner}).should.be.rejectedWith(ERROR_MSG);
+    })
+  })
+
+  describe('executeAffirmation', async () => {
+    let homeBridge;
+    beforeEach(async () => {
+      homeBridge = await HomeBridge.new();
+      await homeBridge.initialize(validatorContract.address, oneEther, halfEther, minPerTx, gasPrice, requireBlockConfirmations, blockRewardContract.address);
+      await blockRewardContract.sendTransaction({
+        from: accounts[2],
+        value: oneEther
+      }).should.be.fulfilled
+    })
+    it('should allow validator to executeAffirmation', async () => {
+      const recipient = accounts[5];
+      const value = halfEther;
+      const balanceBefore = await web3.eth.getBalance(recipient)
+      const balanceBlockRewardContract = await web3.eth.getBalance(blockRewardContract.address)
+      console.log('balanceBlockRewardContract', balanceBlockRewardContract.toString())
+      const transactionHash = "0x806335163828a8eda675cff9c84fa6e6c7cf06bb44cc6ec832e42fe789d01415";
+      const {logs} = await homeBridge.executeAffirmation(recipient, value, transactionHash, {from: authorities[0]})
+
+      logs[0].event.should.be.equal("SignedForAffirmation");
+      logs[0].args.should.be.deep.equal({
+        signer: authorities[0],
+        transactionHash
+      });
+      logs[1].event.should.be.equal("AffirmationCompleted");
+      logs[1].args.should.be.deep.equal({
+        recipient,
+        value,
+        transactionHash
+      })
+      const homeBalanceAfter = await web3.eth.getBalance(homeBridge.address)
+      const balanceAfter = await web3.eth.getBalance(recipient)
+      balanceAfter.should.be.bignumber.equal(balanceBefore.add(value))
+      homeBalanceAfter.should.be.bignumber.equal(0)
+
+      const msgHash = Web3Utils.soliditySha3(recipient, value, transactionHash);
+      const senderHash = Web3Utils.soliditySha3(authorities[0], msgHash)
+      true.should.be.equal(await homeBridge.affirmationsSigned(senderHash))
+    })
+  })
+
+})

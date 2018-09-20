94f7210609bc0aac8ee2186a5fc9fa0c1b99b645
Franco Victorio <victorio.franco@gmail.com>
2018-09-20 13:37:16 -0300

    Add eslint and prettier
    (cherry picked from commit 203d8763cba7f355b6fa25628e8b86f498e7fecc)

--- deploy/src/deploymentUtils.js
+++ deploy/src/deploymentUtils.js
@@ -1,4 +1,8 @@
 const Web3 = require('web3')
+const Tx = require('ethereumjs-tx')
+const Web3Utils = require('web3-utils')
+const fetch = require('node-fetch')
+const assert = require('assert')
 const {
   web3Home,
   web3Foreign,
@@ -8,15 +12,12 @@ const {
   GAS_LIMIT,
   GAS_PRICE,
   GET_RECEIPT_INTERVAL_IN_MILLISECONDS
-} = require('./web3');
-const Tx = require('ethereumjs-tx');
-const Web3Utils = require('web3-utils');
-const fetch = require('node-fetch');
-const assert = require('assert')
+} = require('./web3')
 
-async function deployContract(contractJson, args, {from, network, nonce}) {
-  let web3, url;
-  if(network === 'foreign'){
+async function deployContract(contractJson, args, { from, network, nonce }) {
+  let web3
+  let url
+  if (network === 'foreign') {
     web3 = web3Foreign
     url = FOREIGN_RPC_URL
   } else {
@@ -25,13 +26,15 @@ async function deployContract(contractJson, args, {from, network, nonce}) {
   }
   const options = {
     from,
-    gasPrice: GAS_PRICE,
-  };
-  let instance = new web3.eth.Contract(contractJson.abi, options);
-  const result = await instance.deploy({
-    data: contractJson.bytecode,
-    arguments: args
-  }).encodeABI()
+    gasPrice: GAS_PRICE
+  }
+  const instance = new web3.eth.Contract(contractJson.abi, options)
+  const result = await instance
+    .deploy({
+      data: contractJson.bytecode,
+      arguments: args
+    })
+    .encodeABI()
   const tx = await sendRawTx({
     data: result,
     nonce: Web3Utils.toHex(nonce),
@@ -39,18 +42,17 @@ async function deployContract(contractJson, args, {from, network, nonce}) {
     privateKey: deploymentPrivateKey,
     url
   })
-  if(tx.status !== '0x1'){
-    throw new Error('Tx failed');
+  if (tx.status !== '0x1') {
+    throw new Error('Tx failed')
   }
-  instance.options.address = tx.contractAddress;
+  instance.options.address = tx.contractAddress
   instance.deployedBlockNumber = tx.blockNumber
-  return instance;
+  return instance
 }
 
-
-async function sendRawTx({data, nonce, to, privateKey, url}) {
+async function sendRawTx({ data, nonce, to, privateKey, url }) {
   try {
-    var rawTx = {
+    const rawTx = {
       nonce,
       gasPrice: Web3Utils.toHex(GAS_PRICE),
       gasLimit: Web3Utils.toHex(GAS_LIMIT),
@@ -58,51 +60,53 @@ async function sendRawTx({data, nonce, to, privateKey, url}) {
       data
     }
 
-    var tx = new Tx(rawTx);
-    tx.sign(privateKey);
-    var serializedTx = tx.serialize();
-    const txHash = await sendNodeRequest(url, "eth_sendRawTransaction", '0x' + serializedTx.toString('hex'));
-    console.log('pending txHash', txHash );
-    const receipt = await getReceipt(txHash, url);
+    const tx = new Tx(rawTx)
+    tx.sign(privateKey)
+    const serializedTx = tx.serialize()
+    const txHash = await sendNodeRequest(
+      url,
+      'eth_sendRawTransaction',
+      `0x${serializedTx.toString('hex')}`
+    )
+    console.log('pending txHash', txHash)
+    const receipt = await getReceipt(txHash, url)
     return receipt
-
   } catch (e) {
     console.error(e)
   }
 }
 
-async function sendNodeRequest(url, method, signedData){
+async function sendNodeRequest(url, method, signedData) {
   const request = await fetch(url, {
     headers: {
       'Content-type': 'application/json'
     },
     method: 'POST',
     body: JSON.stringify({
-      jsonrpc: "2.0",
+      jsonrpc: '2.0',
       method,
       params: [signedData],
       id: 1
     })
-  });
+  })
   const json = await request.json()
-  if(method === 'eth_sendRawTransaction') {
+  if (method === 'eth_sendRawTransaction') {
     assert.equal(json.result.length, 66, `Tx wasn't sent ${json}`)
   }
-  return json.result;
-
+  return json.result
 }
 
 function timeout(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 async function getReceipt(txHash, url) {
-  await timeout(GET_RECEIPT_INTERVAL_IN_MILLISECONDS);
-  let receipt = await sendNodeRequest(url, "eth_getTransactionReceipt", txHash);
-  if(receipt === null || receipt.blockNumber === null) {
-    receipt = await getReceipt(txHash, url);
+  await timeout(GET_RECEIPT_INTERVAL_IN_MILLISECONDS)
+  let receipt = await sendNodeRequest(url, 'eth_getTransactionReceipt', txHash)
+  if (receipt === null || receipt.blockNumber === null) {
+    receipt = await getReceipt(txHash, url)
   }
-  return receipt;
+  return receipt
 }
 
 function add0xPrefix(s) {
@@ -110,7 +114,7 @@ function add0xPrefix(s) {
     return s
   }
 
-  return '0x' + s
+  return `0x${s}`
 }
 
 function privateKeyToAddress(privateKey) {

817d1c73c919dc950aa99303d1e52e7f3605cce8
Franco Victorio <victorio.franco@gmail.com>
2018-09-25 13:06:45 -0300

    Add totalMintedCoins to IBlockReward

--- contracts/IBlockReward.sol
+++ contracts/IBlockReward.sol
@@ -3,4 +3,5 @@ pragma solidity 0.4.24;
 
 interface IBlockReward {
     function addExtraReceiver(uint256 _amount, address _receiver) external;
+    function totalMintedCoins() public view returns (uint256);
 }

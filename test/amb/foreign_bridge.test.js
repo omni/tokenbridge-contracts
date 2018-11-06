dd5c7cf3540b2a5c53498abc410bb62ca695e97d
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2018-11-06 13:37:05 -0300

    Add executeSignatures tests for ForeignAMB

--- test/amb/foreign_bridge.test.js
+++ test/amb/foreign_bridge.test.js
@@ -5,6 +5,7 @@ const Box = artifacts.require("Box.sol");
 const requiredBlockConfirmations = 8;
 const gasPrice = Web3Utils.toWei('1', 'gwei');
 const oneEther = web3.toBigNumber(web3.toWei(1, "ether"));
+const { sign, signatureToVRS, strip0x } = require('../helpers/helpers');
 
 const {ERROR_MSG, ZERO_ADDRESS} = require('../setup');
 
@@ -105,6 +106,92 @@ contract('ForeignAMB', async (accounts) => {
         , { from: accounts[3] })
       const { logs } = await web3.eth.getTransactionReceipt(tx)
       logs.length.should.be.equal(1)
+    })
+  })
+  describe('executeSignatures', () => {
+    let foreignBridge, setValueData, box
+    beforeEach(async () => {
+      foreignBridge = await ForeignBridge.new()
+      await foreignBridge.initialize(validatorContract.address, oneEther, gasPrice, requiredBlockConfirmations)
+
+      box = await Box.new()
+      // Generate data for method we want to call on Box contract
+      const result = await box.getSetValueData(3)
+      setValueData = result.logs[0].args.selectorData
+    })
+    it('should succeed on Subsidized mode', async () => {
+      // set foreign bridge on subsidized mode
+      await foreignBridge.setSubsidizedModeForHomeToForeign().should.be.fulfilled;
+
+      const user = accounts[8]
+
+      const boxInitialValue = await box.value()
+      boxInitialValue.should.be.bignumber.equal(0)
+
+      // Use these calls to simulate home bridge on Home network
+      await foreignBridge.setSubsidizedModeForForeignToHome().should.be.fulfilled;
+      const resultPassMessageTx = await foreignBridge.requireToPassMessage(
+        box.address,
+        setValueData,
+        1221254,
+        1, { from: user })
+
+      // Validator on token-bridge add txHash to message
+      const { encodedData } = resultPassMessageTx.logs[0].args
+      const message = encodedData.slice(0, 82) + strip0x(resultPassMessageTx.tx) + encodedData.slice(82)
+
+      const signature = await sign(authorities[0], message)
+      const vrs = signatureToVRS(signature);
+
+      const { logs } = await foreignBridge.executeSignatures(message, [vrs.v], [vrs.r], [vrs.s], {from: authorities[0]})
+      logs[0].event.should.be.equal("RelayedMessage");
+      logs[0].args.should.be.deep.equal({
+        sender: user,
+        executor: box.address,
+        transactionHash: resultPassMessageTx.tx
+      })
+
+      //check Box value
+      const boxValue = await box.value()
+      boxValue.should.be.bignumber.equal(3)
+    })
+    it('should succeed on defrayal mode', async () => {
+      const user = accounts[8]
+
+      const boxInitialValue = await box.value()
+      boxInitialValue.should.be.bignumber.equal(0)
+
+      // Deposit for user
+      await foreignBridge.depositForContractSender(user, {
+        from: user,
+        value: oneEther
+      })
+
+      // Use these calls to simulate foreign bridge on Foreign network
+      const resultPassMessageTx = await foreignBridge.requireToPassMessage(
+        box.address,
+        setValueData,
+        821254,
+        1, {from: user})
+
+      // Validator on token-bridge add txHash to message
+      const { encodedData } = resultPassMessageTx.logs[0].args
+      const message = encodedData.slice(0, 82) + strip0x(resultPassMessageTx.tx) + encodedData.slice(82)
+
+      const signature = await sign(authorities[0], message)
+      const vrs = signatureToVRS(signature);
+      console.log("message length", message.length)
+      const {logs} = await foreignBridge.executeSignatures(message, [vrs.v], [vrs.r], [vrs.s], {from: authorities[0]})
+      logs[0].event.should.be.equal("RelayedMessage");
+      logs[0].args.should.be.deep.equal({
+        sender: user,
+        executor: box.address,
+        transactionHash: resultPassMessageTx.tx
+      })
+
+      //check Box value
+      const boxValue = await box.value()
+      boxValue.should.be.bignumber.equal(3)
     })
   })
 })

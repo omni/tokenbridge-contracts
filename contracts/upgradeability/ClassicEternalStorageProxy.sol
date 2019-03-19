a5bf24bf210762c035b07b38689f5cca68e8ec67
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2019-03-19 11:21:02 -0300

    remove comments on ClassicEternalStorageProxy

--- contracts/upgradeability/ClassicEternalStorageProxy.sol
+++ contracts/upgradeability/ClassicEternalStorageProxy.sol
@@ -4,12 +4,6 @@ import "./EternalStorage.sol";
 import "./OwnedUpgradeabilityProxy.sol";
 
 
-/**
- * @title EternalStorageProxy
- * @dev This proxy holds the storage of the token contract and delegates every call to the current implementation set.
- * Besides, it allows to upgrade the token's behaviour towards further implementations, and provides basic
- * authorization control functionalities
- */
 contract ClassicEternalStorageProxy is OwnedUpgradeabilityProxy, EternalStorage {
 
     function () payable public {

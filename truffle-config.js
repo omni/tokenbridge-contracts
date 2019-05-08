d5139e164a4c66cb1641806b17f53424a69f1af3
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2019-05-08 10:42:57 -0300

    Upgrade truffle version

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,36 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    coverage: {
+      host: 'localhost',
+      network_id: '*', // eslint-disable-line camelcase
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
+    },
+    ganache: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*', // eslint-disable-line camelcase
+      gasPrice: 100000000000
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.24",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
+        evmVersion: "byzantium"
+      }
+    }
+  },
+  mocha: {
+    reporter: 'eth-gas-reporter',
+     reporterOptions : {
+       currency: 'USD',
+       gasPrice: 1
+     }
+  }
 };

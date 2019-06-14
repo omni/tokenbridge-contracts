9fa1455cde3bc887259feae93e2f02e755be6f9d
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2019-06-14 17:06:19 -0300

    remove not used network config

--- truffle-config.js
+++ truffle-config.js
@@ -51,13 +51,6 @@ module.exports = {
       provider,
       network_id: '*'
     },
-    coverage: {
-      host: 'localhost',
-      network_id: '*', // eslint-disable-line camelcase
-      port: 8555,
-      gas: 0xfffffffffff,
-      gasPrice: 0x01
-    },
     ganache: {
       host: '127.0.0.1',
       port: 8545,

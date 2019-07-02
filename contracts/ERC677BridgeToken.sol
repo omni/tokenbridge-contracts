94c3e02364ea570d953054341e17707aef5230bd
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2019-07-02 09:58:30 -0300

    Update token interface version

--- contracts/ERC677BridgeToken.sol
+++ contracts/ERC677BridgeToken.sol
@@ -46,7 +46,7 @@ contract ERC677BridgeToken is
     }
 
     function getTokenInterfacesVersion() public pure returns(uint64 major, uint64 minor, uint64 patch) {
-        return (2, 0, 0);
+        return (2, 1, 0);
     }
 
     function superTransfer(address _to, uint256 _value) internal returns(bool)

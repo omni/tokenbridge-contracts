12885280385f0c9963d9b458f91d0e9fbe297648
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2019-07-29 15:37:31 -0300

    Increase version in BaseBridgeValidators

--- contracts/upgradeable_contracts/BaseBridgeValidators.sol
+++ contracts/upgradeable_contracts/BaseBridgeValidators.sol
@@ -22,7 +22,7 @@ contract BaseBridgeValidators is EternalStorage, Ownable {
     }
 
     function getBridgeValidatorsInterfacesVersion() external pure returns (uint64 major, uint64 minor, uint64 patch) {
-        return (2, 2, 0);
+        return (2, 2, 1);
     }
 
     function validatorList() external view returns (address[]) {

5956590c6679b48d793005ffbe240752905e0650
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2019-08-05 15:25:49 -0300

    Replace method signature with constant selector in BlockRewardBridge

--- contracts/upgradeable_contracts/BlockRewardBridge.sol
+++ contracts/upgradeable_contracts/BlockRewardBridge.sol
@@ -6,6 +6,8 @@ import "openzeppelin-solidity/contracts/AddressUtils.sol";
 
 contract BlockRewardBridge is EternalStorage {
     bytes32 internal constant BLOCK_REWARD_CONTRACT = keccak256(abi.encodePacked("blockRewardContract"));
+    bytes4 internal constant BLOCK_REWARD_CONTRACT_ID = 0x2ee57f8d; // blockRewardContractId()
+    bytes4 internal constant BRIDGES_ALLOWED_LENGTH = 0x10f2ee7c; // bridgesAllowedLength()
 
     function _blockRewardContract() internal view returns (IBlockReward) {
         return IBlockReward(addressStorage[BLOCK_REWARD_CONTRACT]);
@@ -17,10 +19,10 @@ contract BlockRewardBridge is EternalStorage {
         // Before store the contract we need to make sure that it is the block reward contract in actual fact,
         // call a specific method from the contract that should return a specific value
         bool isBlockRewardContract = false;
-        if (_blockReward.call(abi.encodeWithSignature("blockRewardContractId()"))) {
+        if (_blockReward.call(BLOCK_REWARD_CONTRACT_ID)) {
             isBlockRewardContract =
                 IBlockReward(_blockReward).blockRewardContractId() == bytes4(keccak256("blockReward"));
-        } else if (_blockReward.call(abi.encodeWithSignature("bridgesAllowedLength()"))) {
+        } else if (_blockReward.call(BRIDGES_ALLOWED_LENGTH)) {
             isBlockRewardContract = IBlockReward(_blockReward).bridgesAllowedLength() != 0;
         }
         require(isBlockRewardContract);

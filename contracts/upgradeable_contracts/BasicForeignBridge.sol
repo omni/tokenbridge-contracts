cf3e507b4799b6b4b426b9fa2956d6f5abd994d4
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2019-08-12 16:31:43 -0300

    Extract common logic from basic foreign contracts

--- contracts/upgradeable_contracts/BasicForeignBridge.sol
+++ contracts/upgradeable_contracts/BasicForeignBridge.sol
@@ -5,8 +5,9 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Basic.sol";
 import "./Validatable.sol";
 import "../libraries/Message.sol";
 import "./BasicTokenBridge.sol";
+import "./MessageRelay.sol";
 
-contract BasicForeignBridge is EternalStorage, Validatable, BasicTokenBridge {
+contract BasicForeignBridge is EternalStorage, Validatable, BasicTokenBridge, MessageRelay {
     /// triggered when relay of deposit from HomeBridge is complete
     event RelayedMessage(address recipient, uint256 value, bytes32 transactionHash);
     function executeSignatures(uint8[] vs, bytes32[] rs, bytes32[] ss, bytes message) external {
@@ -29,14 +30,6 @@ contract BasicForeignBridge is EternalStorage, Validatable, BasicTokenBridge {
 
     /* solcov ignore next */
     function onExecuteMessage(address, uint256, bytes32) internal returns (bool);
-
-    function setRelayedMessages(bytes32 _txHash, bool _status) internal {
-        boolStorage[keccak256(abi.encodePacked("relayedMessages", _txHash))] = _status;
-    }
-
-    function relayedMessages(bytes32 _txHash) public view returns (bool) {
-        return boolStorage[keccak256(abi.encodePacked("relayedMessages", _txHash))];
-    }
 
     /* solcov ignore next */
     function onFailedMessage(address, uint256, bytes32) internal;
--- contracts/upgradeable_contracts/arbitrary_message/BasicForeignAMB.sol
+++ contracts/upgradeable_contracts/arbitrary_message/BasicForeignAMB.sol
@@ -4,20 +4,12 @@ import "../../libraries/Message.sol";
 import "./BasicAMB.sol";
 import "./MessageDelivery.sol";
 import "./MessageProcessor.sol";
+import "../MessageRelay.sol";
 
-contract BasicForeignAMB is BasicAMB, MessageDelivery, MessageProcessor {
+contract BasicForeignAMB is BasicAMB, MessageRelay, MessageDelivery, MessageProcessor {
     function executeSignatures(bytes _data, uint8[] vs, bytes32[] rs, bytes32[] ss) external {
         Message.hasEnoughValidSignatures(_data, vs, rs, ss, validatorContract(), true);
 
         processMessage(_data, true);
     }
-
-    function relayedMessages(bytes32 _txHash) public view returns (bool) {
-        return boolStorage[keccak256(abi.encodePacked("relayedMessages", _txHash))];
-    }
-
-    function setRelayedMessages(bytes32 _txHash, bool _status) internal {
-        boolStorage[keccak256(abi.encodePacked("relayedMessages", _txHash))] = _status;
-    }
-
 }

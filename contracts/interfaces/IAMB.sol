3b2a272e6f6704685b0c181022e3e28bf1861a26
Gerardo Nardelli <gerardo.nardelli@altoros.com>
2019-09-11 17:10:10 -0300

    Fix IAMB

--- contracts/interfaces/IAMB.sol
+++ contracts/interfaces/IAMB.sol
@@ -4,7 +4,7 @@ interface IAMB {
     function messageSender() external view returns (address);
     function maxGasPerTx() external view returns (uint256);
     function transactionHash() external view returns (bytes32);
-    function messageCallStatus(bytes32 _txHash) external view returns (address);
+    function messageCallStatus(bytes32 _txHash) external view returns (bool);
     function failedMessageDataHash(bytes32 _txHash) external view returns (bytes32);
     function failedMessageReceiver(bytes32 _txHash) external view returns (address);
     function failedMessageSender(bytes32 _txHash) external view returns (address);

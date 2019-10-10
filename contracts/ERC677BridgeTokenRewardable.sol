9a858d537bdc4bcc016df2d6abcbffbea427a6b3
Vadim <varasev@gmail.com>
2019-10-10 18:07:00 +0300

    Simplify mintReward function

--- contracts/ERC677BridgeTokenRewardable.sol
+++ contracts/ERC677BridgeTokenRewardable.sol
@@ -33,21 +33,14 @@ contract ERC677BridgeTokenRewardable is ERC677BridgeToken {
         _;
     }
 
-    function mintReward(address[] _receivers, uint256[] _rewards) external onlyBlockRewardContract {
-        uint256 receiversLength = _receivers.length;
-        for (uint256 i = 0; i < receiversLength; i++) {
-            uint256 amount = _rewards[i];
-
-            if (amount == 0) continue;
-
-            address to = _receivers[i];
-
-            // Mint `amount` for `to`
-            totalSupply_ = totalSupply_.add(amount);
-            balances[to] = balances[to].add(amount);
-            emit Mint(to, amount);
-            emit Transfer(address(0), to, amount);
-        }
+    function mintReward(uint256 _amount) external onlyBlockRewardContract {
+        if (_amount == 0) return;
+        // Mint `_amount` for the BlockReward contract
+        address to = blockRewardContract;
+        totalSupply_ = totalSupply_.add(_amount);
+        balances[to] = balances[to].add(_amount);
+        emit Mint(to, _amount);
+        emit Transfer(address(0), to, _amount);
     }
 
     function stake(address _staker, uint256 _amount) external onlyStakingContract {
@@ -58,11 +51,13 @@ contract ERC677BridgeTokenRewardable is ERC677BridgeToken {
     }
 
     function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != blockRewardContract);
         require(_to != stakingContract);
         return super.transfer(_to, _value);
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_to != blockRewardContract);
         require(_to != stakingContract);
         return super.transferFrom(_from, _to, _value);
     }

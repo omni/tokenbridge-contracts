f7850fa222ecf8b61f6be3cf05f7135c2987ad13
Max Alekseenko <nnwen0609@gmail.com>
2019-10-29 21:40:16 +0300

    remove checks for relative limit

--- contracts/upgradeable_contracts/RelativeDailyLimit.sol
+++ contracts/upgradeable_contracts/RelativeDailyLimit.sol
@@ -10,4 +10,14 @@ contract RelativeDailyLimit is BaseRelativeDailyLimit {
     function dailyLimit() public view returns (uint256) {
         return _getTodayLimit();
     }
+
+    function setMinPerTx(uint256 _minPerTx) external onlyOwner {
+        require(_minPerTx < maxPerTx());
+        uintStorage[MIN_PER_TX] = _minPerTx;
+    }
+
+    function setMaxPerTx(uint256 _maxPerTx) external onlyOwner {
+        require(_maxPerTx > minPerTx());
+        uintStorage[MAX_PER_TX] = _maxPerTx;
+    }
 }
--- contracts/upgradeable_contracts/RelativeExecutionDailyLimit.sol
+++ contracts/upgradeable_contracts/RelativeExecutionDailyLimit.sol
@@ -10,4 +10,14 @@ contract RelativeExecutionDailyLimit is BaseRelativeDailyLimit {
     function executionDailyLimit() public view returns (uint256) {
         return _getTodayLimit();
     }
+
+    function setExecutionMinPerTx(uint256 _minPerTx) external onlyOwner {
+        require(_minPerTx < executionMaxPerTx());
+        uintStorage[EXECUTION_MIN_PER_TX] = _minPerTx;
+    }
+
+    function setExecutionMaxPerTx(uint256 _maxPerTx) external onlyOwner {
+        require(_maxPerTx > executionMinPerTx());
+        uintStorage[EXECUTION_MAX_PER_TX] = _maxPerTx;
+    }
 }

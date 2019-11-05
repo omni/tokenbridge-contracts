26046a162fb073dffb054c61d9c903af0bdd14c4
Max Alekseenko <nnwen0609@gmail.com>
2019-11-05 17:41:44 +0300

    add a multiplier to the daily limit calculation

--- contracts/upgradeable_contracts/BaseRelativeDailyLimit.sol
+++ contracts/upgradeable_contracts/BaseRelativeDailyLimit.sol
@@ -21,15 +21,16 @@ contract BaseRelativeDailyLimit is BasicTokenBridge {
         }
         uint256 limit = targetLimit();
         uint256 thresh = threshold();
+        uint256 multiplier = 1 ether ** 2;
         if (balance > unlimitedBalance && balance < thresh) {
             // to save the gas we don't need to use safe math here
             // because we check in setters that limit is always less than 1 ether
             // and threshold is greater than minPerTx
             // and minPerTx is less than threshold
-            uint256 a = (1 ether - limit) / (thresh - unlimitedBalance)**2;
+            uint256 a = ((1 ether - limit) * multiplier) / (thresh - unlimitedBalance)**2;
             uint256 b = 2 * a * thresh;
-            uint256 c = limit + a * thresh**2;
-            limit = a * balance**2 - b * balance + c;
+            uint256 c = (limit * multiplier) + a * thresh**2;
+            limit = (a * balance**2 - b * balance + c) / multiplier;
         }
         return (balance * limit) / 1 ether;
     }

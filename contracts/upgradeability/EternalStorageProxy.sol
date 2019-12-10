4eefa92c2d8176d5eb001d4ec0ad90359804eb4c
Kirill Fedoseev <k.fedoseev@innopolis.university>
2019-12-10 14:12:57 +0300

    isContract check during upgrade, as it was done in a local version of upgradeability contract

--- contracts/upgradeability/EternalStorageProxy.sol
+++ contracts/upgradeability/EternalStorageProxy.sol
@@ -1,6 +1,12 @@
 pragma solidity 0.4.24;
 
+import "openzeppelin-solidity/contracts/AddressUtils.sol";
 import "openzeppelin-labs/upgradeability_using_eternal_storage/contracts/EternalStorage.sol";
 import "openzeppelin-labs/upgradeability_using_eternal_storage/contracts/OwnedUpgradeabilityProxy.sol";
 
-contract EternalStorageProxy is EternalStorage, OwnedUpgradeabilityProxy {}
+contract EternalStorageProxy is EternalStorage, OwnedUpgradeabilityProxy {
+    function upgradeTo(string version, address implementation) public {
+        require(AddressUtils.isContract(implementation));
+        super.upgradeTo(version, implementation);
+    }
+}

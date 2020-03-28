bfea462cd6830a0a7f0bf80bf3c1f8aee93cf432
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-03-28 22:04:59 +0300

    Update interest receiver (#382)

--- deploy/src/utils/deployInterestReceiver.js
+++ deploy/src/utils/deployInterestReceiver.js
@@ -1,9 +1,7 @@
-const assert = require('assert')
-const Web3Utils = require('web3-utils')
 const env = require('../loadEnv')
 
-const { deployContract, privateKeyToAddress, sendRawTxForeign } = require('../deploymentUtils')
-const { web3Foreign, deploymentPrivateKey, FOREIGN_RPC_URL } = require('../web3')
+const { deployContract, privateKeyToAddress } = require('../deploymentUtils')
+const { web3Foreign } = require('../web3')
 
 const {
   foreignContracts: { InterestReceiver }
@@ -18,28 +16,14 @@ const DEPLOYMENT_ACCOUNT_ADDRESS = privateKeyToAddress(DEPLOYMENT_ACCOUNT_PRIVAT
 
 async function deployInterestReceiver() {
   let foreignNonce = await web3Foreign.eth.getTransactionCount(DEPLOYMENT_ACCOUNT_ADDRESS)
-  console.log('\n[Foreign] deploying Interest receiver contract')
+  console.log('\n[Foreign] deploying Interest receiver contract, setting owner to ', FOREIGN_INTEREST_RECEIVER_OWNER)
   const interestReceiver = await deployContract(
     InterestReceiver,
-    [],
+    [FOREIGN_INTEREST_RECEIVER_OWNER],
     { from: DEPLOYMENT_ACCOUNT_ADDRESS, network: 'foreign', nonce: foreignNonce }
   )
   foreignNonce++
   console.log('[Foreign] Interest receiver: ', interestReceiver.options.address)
-
-  console.log('[Foreign] initializing contract owner to ', FOREIGN_INTEREST_RECEIVER_OWNER)
-  const initializeData = await interestReceiver.methods
-    .initialize(FOREIGN_INTEREST_RECEIVER_OWNER)
-    .encodeABI({ from: DEPLOYMENT_ACCOUNT_ADDRESS })
-  const txInitialize = await sendRawTxForeign({
-    data: initializeData,
-    nonce: foreignNonce,
-    to: interestReceiver.options.address,
-    privateKey: deploymentPrivateKey,
-    url: FOREIGN_RPC_URL
-  })
-  assert.strictEqual(Web3Utils.hexToNumber(txInitialize.status), 1, 'Transaction Failed')
-
   console.log('\nInterest receiver deployment is completed\n')
   return {
     interestReceiverAddress: interestReceiver.options.address

e158bf87a985370805711b4057b0fccfed6ac603
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-04-03 14:16:11 +0300

    Simplify mint+transfer approach in the foreign chain

--- contracts/upgradeable_contracts/stake_token_mediators/ForeignStakeTokenMediator.sol
+++ contracts/upgradeable_contracts/stake_token_mediators/ForeignStakeTokenMediator.sol
@@ -11,8 +11,7 @@ contract ForeignStakeTokenMediator is BasicStakeTokenMediator {
      */
     function executeActionOnBridgedTokens(address _recipient, uint256 _value) internal {
         uint256 value = _value.div(10**decimalShift());
-        _mintLackingTokens(value);
-        erc677token().transfer(_recipient, value);
+        executeActionOnFixedTokens(_recipient, value);
     }
 
     /**
@@ -38,21 +37,15 @@ contract ForeignStakeTokenMediator is BasicStakeTokenMediator {
      * @param _value amount of fixed tokens
      */
     function executeActionOnFixedTokens(address _recipient, uint256 _value) internal {
-        _mintLackingTokens(_value);
-        erc677token().transfer(_recipient, _value);
-    }
-
-    /**
-     * @dev Mints tokens if the bridge balance is insufficient.
-     * This can happen after STAKE tokens emission in the xDai chain
-     * @param _value amount of tokens, which are requested to be withdrawn
-     */
-    function _mintLackingTokens(uint256 _value) internal {
         IBurnableMintableERC677Token token = IBurnableMintableERC677Token(erc677token());
         uint256 balance = token.balanceOf(address(this));
-        if (balance < _value) {
-            // if bridge balance is less than requested value, the lacking part of tokens will be minted
+        if (_recipient != address(0) && balance == 0) {
+            token.mint(_recipient, _value);
+        } else if (balance < _value) {
             token.mint(address(this), _value - balance);
+            token.transfer(_recipient, _value);
+        } else {
+            token.transfer(_recipient, _value);
         }
     }
 }

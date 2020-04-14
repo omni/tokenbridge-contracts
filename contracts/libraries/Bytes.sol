155836436cbfef4f2af5544529b62b46e0e7e609
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-04-14 15:20:04 +0300

    Remove bytesToBytes32

--- contracts/libraries/Bytes.sol
+++ contracts/libraries/Bytes.sol
@@ -6,18 +6,6 @@ pragma solidity 0.4.24;
  */
 library Bytes {
     /**
-    * @dev Truncate bytes array if its size is more than 32 bytes
-    * and pads the array with zeros from the right side if its size is less than 32.
-    * @param _bytes to be converted to bytes32 type
-    * @return bytes32 type of the firsts 32 bytes array in parameter.
-    */
-    function bytesToBytes32(bytes _bytes) internal pure returns (bytes32 result) {
-        assembly {
-            result := mload(add(_bytes, 32))
-        }
-    }
-
-    /**
     * @dev Truncate bytes array if its size is more than 20 bytes
     * and pads the array with zeros from the right side if its size is less than 20.
     * @param _bytes to be converted to address type

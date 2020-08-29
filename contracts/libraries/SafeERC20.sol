3f1f04d7dd7a973f463514a8bff2945ea62ac7d0
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-08-29 20:30:05 +0300

    Extra check for balance increase (#479)

---
+++ contracts/libraries/SafeERC20.sol
@@ -0,0 +1,49 @@
+pragma solidity 0.4.24;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "../interfaces/ERC677.sol";
+
+/**
+ * @title SafeERC20
+ * @dev Helper methods for safe token transfers.
+ * Functions perform additional checks to be sure that token transfer really happened.
+ */
+library SafeERC20 {
+    using SafeMath for uint256;
+
+    /**
+    * @dev Same as ERC20.transfer(address,uint256) but with extra consistency checks.
+    * @param _token address of the token contract
+    * @param _to address of the receiver
+    * @param _value amount of tokens to send
+    */
+    function safeTransfer(address _token, address _to, uint256 _value) internal {
+        LegacyERC20(_token).transfer(_to, _value);
+        assembly {
+            if returndatasize {
+                returndatacopy(0, 0, 32)
+                if iszero(mload(0)) {
+                    revert(0, 0)
+                }
+            }
+        }
+    }
+
+    /**
+    * @dev Same as ERC20.transferFrom(address,address,uint256) but with extra consistency checks.
+    * @param _token address of the token contract
+    * @param _from address of the sender
+    * @param _value amount of tokens to send
+    */
+    function safeTransferFrom(address _token, address _from, uint256 _value) internal {
+        LegacyERC20(_token).transferFrom(_from, address(this), _value);
+        assembly {
+            if returndatasize {
+                returndatacopy(0, 0, 32)
+                if iszero(mload(0)) {
+                    revert(0, 0)
+                }
+            }
+        }
+    }
+}
--- contracts/upgradeable_contracts/amb_erc677_to_erc677/ForeignAMBErc677ToErc677.sol
+++ contracts/upgradeable_contracts/amb_erc677_to_erc677/ForeignAMBErc677ToErc677.sol
@@ -1,6 +1,7 @@
 pragma solidity 0.4.24;
 
 import "./BasicAMBErc677ToErc677.sol";
+import "../../libraries/SafeERC20.sol";
 
 /**
 * @title ForeignAMBErc677ToErc677
@@ -8,6 +9,8 @@ import "./BasicAMBErc677ToErc677.sol";
 * It is designed to be used as an implementation contract of EternalStorageProxy contract.
 */
 contract ForeignAMBErc677ToErc677 is BasicAMBErc677ToErc677 {
+    using SafeERC20 for ERC677;
+
     /**
      * @dev Executes action on the request to withdraw tokens relayed from the other network
      * @param _recipient address of tokens receiver
@@ -16,8 +19,29 @@ contract ForeignAMBErc677ToErc677 is BasicAMBErc677ToErc677 {
     function executeActionOnBridgedTokens(address _recipient, uint256 _value) internal {
         uint256 value = _unshiftValue(_value);
         bytes32 _messageId = messageId();
-        erc677token().transfer(_recipient, value);
+        erc677token().safeTransfer(_recipient, value);
         emit TokensBridged(_recipient, value, _messageId);
+    }
+
+    /**
+    * @dev Initiates the bridge operation that will lock the amount of tokens transferred and mint the tokens on
+    * the other network. The user should first call Approve method of the ERC677 token.
+    * @param _receiver address that will receive the minted tokens on the other network.
+    * @param _value amount of tokens to be transferred to the other network.
+    */
+    function relayTokens(address _receiver, uint256 _value) external {
+        // This lock is to prevent calling passMessage twice if a ERC677 token is used.
+        // When transferFrom is called, after the transfer, the ERC677 token will call onTokenTransfer from this contract
+        // which will call passMessage.
+        require(!lock());
+        ERC677 token = erc677token();
+        require(withinLimit(_value));
+        addTotalSpentPerDay(getCurrentDay(), _value);
+
+        setLock(true);
+        token.safeTransferFrom(msg.sender, _value);
+        setLock(false);
+        bridgeSpecificActionsOnTokenTransfer(token, msg.sender, _value, abi.encodePacked(_receiver));
     }
 
     /**
@@ -38,6 +62,6 @@ contract ForeignAMBErc677ToErc677 is BasicAMBErc677ToErc677 {
     }
 
     function executeActionOnFixedTokens(address _recipient, uint256 _value) internal {
-        erc677token().transfer(_recipient, _value);
+        erc677token().safeTransfer(_recipient, _value);
     }
 }

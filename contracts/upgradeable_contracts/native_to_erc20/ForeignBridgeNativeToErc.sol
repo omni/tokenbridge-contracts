109d05352d6d10ee110680c8cd0850e099a37e78
Kirill Fedoseev <k.fedoseev@innopolis.university>
2020-10-30 09:55:16 +0300

    Remove unneeded sub operation in case of zero fee

--- contracts/upgradeable_contracts/native_to_erc20/ForeignBridgeNativeToErc.sol
+++ contracts/upgradeable_contracts/native_to_erc20/ForeignBridgeNativeToErc.sol
@@ -124,8 +124,10 @@ contract ForeignBridgeNativeToErc is
         address feeManager = feeManagerContract();
         if (feeManager != address(0)) {
             uint256 fee = calculateFee(valueToMint, feeManager, HOME_FEE);
-            distributeFeeFromSignatures(fee, feeManager, _txHash);
-            valueToMint = valueToMint.sub(fee);
+            if (fee > 0) {
+                distributeFeeFromSignatures(fee, feeManager, _txHash);
+                valueToMint = valueToMint.sub(fee);
+            }
         }
         return IBurnableMintableERC677Token(erc677token()).mint(_recipient, valueToMint);
     }
--- contracts/upgradeable_contracts/native_to_erc20/RewardableForeignBridgeNativeToErc.sol
+++ contracts/upgradeable_contracts/native_to_erc20/RewardableForeignBridgeNativeToErc.sol
@@ -10,4 +10,15 @@ contract RewardableForeignBridgeNativeToErc is RewardableBridge {
     function getHomeFee() public view returns (uint256) {
         return _getFee(HOME_FEE);
     }
+
+    /**
+     * @dev Internal function for distributing the fee for collecting sufficient amount of signatures.
+     * @param _fee amount of fee to distribute.
+     * @param _feeManager address of the fee manager contract.
+     * @param _txHash reference transaction hash where the original bridge request happened.
+     */
+    function distributeFeeFromSignatures(uint256 _fee, address _feeManager, bytes32 _txHash) internal {
+        require(_feeManager.delegatecall(abi.encodeWithSelector(DISTRIBUTE_FEE_FROM_SIGNATURES, _fee)));
+        emit FeeDistributedFromSignatures(_fee, _txHash);
+    }
 }

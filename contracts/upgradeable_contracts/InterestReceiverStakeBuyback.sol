0d346d5839162d30cbd9068ce25b2900f7d14fe6
Kirill Fedoseev <k.fedoseev@innopolis.university>
2021-04-23 11:21:02 +0300

    Add minReceivedFraction param to InterestReceiverSwapToETH

--- contracts/upgradeable_contracts/InterestReceiverStakeBuyback.sol
+++ contracts/upgradeable_contracts/InterestReceiverStakeBuyback.sol
@@ -1,32 +1,16 @@
 pragma solidity 0.4.24;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import "openzeppelin-solidity/contracts/AddressUtils.sol";
-import "../interfaces/IInterestReceiver.sol";
-import "../interfaces/IUniswapRouterV2.sol";
-import "./Claimable.sol";
+import "./InterestReceiverBase.sol";
 
 /**
  * @title InterestReceiverStakeBuyback
  * @dev This contract is intended to be used together with InterestConnector module of the erc-to-native bridge.
  * Contract receives DAI and COMP tokens. All received tokens are swapped to STAKE token and burnt.
  */
-contract InterestReceiverStakeBuyback is IInterestReceiver, Ownable, Claimable {
-    ERC20 public constant daiToken = ERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
-    ERC20 public constant compToken = ERC20(0xc00e94Cb662C3520282E6f5717214004A7f26888);
-    ERC20 public constant wethToken = ERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+contract InterestReceiverStakeBuyback is InterestReceiverBase {
     ERC20 public constant stakeToken = ERC20(0x0Ae055097C6d159879521C384F1D2123D1f195e6);
-    IUniswapRouterV2 public constant uniswapRouterV2 = IUniswapRouterV2(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
 
     address public constant burnAddress = 0x000000000000000000000000000000000000dEaD;
-
-    uint256 public minReceivedFraction = 0;
-
-    constructor() public {
-        daiToken.approve(address(uniswapRouterV2), uint256(-1));
-        compToken.approve(address(uniswapRouterV2), uint256(-1));
-    }
 
     /**
      * @dev Callback function for notifying this contract about received interest.
@@ -51,26 +35,5 @@ contract InterestReceiverStakeBuyback is IInterestReceiver, Ownable, Claimable {
             now
         );
         address(uniswapRouterV2).call(data);
-    }
-
-    /**
-     * @dev Updates the slippage parameter for the Uniswap operations.
-     * Only owner can call this method.
-     * @param _minFraction minimum percentage allowed to be received w.r.t. 1 ether (0.9 ether = 90%),
-     * slippage = 1 ether - minReceivedFraction.
-     */
-    function setMinFractionReceived(uint256 _minFraction) external onlyOwner {
-        require(_minFraction < 1 ether);
-        minReceivedFraction = _minFraction;
-    }
-
-    /**
-    * @dev Allows to transfer any locked token from this contract.
-    * Only owner can call this method.
-    * @param _token address of the token, if it is not provided (0x00..00), native coins will be transferred.
-    * @param _to address that will receive the locked tokens on this contract.
-    */
-    function claimTokens(address _token, address _to) external onlyOwner {
-        claimValues(_token, _to);
     }
 }
--- contracts/upgradeable_contracts/InterestReceiverSwapToETH.sol
+++ contracts/upgradeable_contracts/InterestReceiverSwapToETH.sol
@@ -1,11 +1,6 @@
 pragma solidity 0.4.24;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import "openzeppelin-solidity/contracts/AddressUtils.sol";
-import "../interfaces/IInterestReceiver.sol";
-import "../interfaces/IUniswapRouterV2.sol";
-import "./Claimable.sol";
+import "./InterestReceiverBase.sol";
 
 /**
  * @title InterestReceiverSwapToETH
@@ -13,17 +8,7 @@ import "./Claimable.sol";
  * Contract receives DAI and COMP tokens. All received tokens are swapped to ETH and kept on this contract.
  * Later, they can be withdrawn by the owner.
  */
-contract InterestReceiverSwapToETH is IInterestReceiver, Ownable, Claimable {
-    ERC20 public constant daiToken = ERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
-    ERC20 public constant compToken = ERC20(0xc00e94Cb662C3520282E6f5717214004A7f26888);
-    ERC20 public constant wethToken = ERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-    IUniswapRouterV2 public constant uniswapRouterV2 = IUniswapRouterV2(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
-
-    constructor() public {
-        daiToken.approve(address(uniswapRouterV2), uint256(-1));
-        compToken.approve(address(uniswapRouterV2), uint256(-1));
-    }
-
+contract InterestReceiverSwapToETH is InterestReceiverBase {
     /**
      * @dev Callback function for notifying this contract about received interest.
      * @param _token address of the token contract. Should be COMP or DAI token address.
@@ -33,21 +18,23 @@ contract InterestReceiverSwapToETH is IInterestReceiver, Ownable, Claimable {
         path[0] = _token;
         path[1] = wethToken;
         uint256 amount = ERC20(_token).balanceOf(address(this));
-        uniswapRouterV2.swapExactTokensForETH(amount, 0, path, address(this), now);
+
+        // (min received %) * (amount / 1 DAI) * (ETH per 1 DAI)
+        uint256 minAmount = (minReceivedFraction * amount * uniswapRouterV2.getAmountsOut(1 ether, path)[1]) / 10**36;
+
+        bytes memory data = abi.encodeWithSelector(
+            uniswapRouterV2.swapExactTokensForETH.selector,
+            amount,
+            minAmount,
+            path,
+            address(this),
+            now
+        );
+        address(uniswapRouterV2).call(data);
     }
 
     /**
-    * @dev Allows to transfer any locked token from this contract.
-    * Only owner can call this method.
-    * @param _token address of the token, if it is not provided (0x00..00), native coins will be transferred.
-    * @param _to address that will receive the locked tokens on this contract.
-    */
-    function claimTokens(address _token, address _to) external onlyOwner {
-        claimValues(_token, _to);
-    }
-
-    /**
-    * @dev Fallback function for receiving native coins from the Uniswap Router contract when using swapExactTokensForETH.
-    */
+     * @dev Fallback function for receiving native coins from the Uniswap Router contract when using swapExactTokensForETH.
+     */
     function() external payable {}
 }

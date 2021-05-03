aa771ecc7e4bbb21c5a948c57611d2fa0fd28bd8
Kirill Fedoseev <k.fedoseev@innopolis.university>
2021-05-03 16:58:57 +0300

    Remove deprecated bridge types (#595)

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -11,51 +11,6 @@ function writeDeploymentResults(data) {
   console.log('Contracts Deployment have been saved to `bridgeDeploymentResults.json`')
 }
 
-async function deployNativeToErc() {
-  const preDeploy = require('./src/native_to_erc/preDeploy')
-  const deployHome = require('./src/native_to_erc/home')
-  const deployForeign = require('./src/native_to_erc/foreign')
-  await preDeploy()
-  const { homeBridge } = await deployHome()
-  const { foreignBridge, erc677 } = await deployForeign(homeBridge.address)
-  console.log('\nDeployment has been completed.\n\n')
-  console.log(`[   Home  ] HomeBridge: ${homeBridge.address} at block ${homeBridge.deployedBlockNumber}`)
-  console.log(`[ Foreign ] ForeignBridge: ${foreignBridge.address} at block ${foreignBridge.deployedBlockNumber}`)
-  console.log(`[ Foreign ] ERC677 Bridgeable Token: ${erc677.address}`)
-  writeDeploymentResults({
-    homeBridge: {
-      ...homeBridge
-    },
-    foreignBridge: {
-      ...foreignBridge,
-      erc677
-    }
-  })
-}
-
-async function deployErcToErc() {
-  const preDeploy = require('./src/erc_to_erc/preDeploy')
-  const deployHome = require('./src/erc_to_erc/home')
-  const deployForeign = require('./src/erc_to_erc/foreign')
-  await preDeploy()
-  const { homeBridge, erc677 } = await deployHome()
-  const { foreignBridge } = await deployForeign()
-  console.log('\nDeployment has been completed.\n\n')
-  console.log(`[   Home  ] HomeBridge: ${homeBridge.address} at block ${homeBridge.deployedBlockNumber}`)
-  console.log(`[   Home  ] ERC677 Bridgeable Token: ${erc677.address}`)
-  console.log(`[ Foreign ] ForeignBridge: ${foreignBridge.address} at block ${foreignBridge.deployedBlockNumber}`)
-  console.log(`[ Foreign ] ERC20 Token: ${ERC20_TOKEN_ADDRESS}`)
-  writeDeploymentResults({
-    homeBridge: {
-      ...homeBridge,
-      erc677
-    },
-    foreignBridge: {
-      ...foreignBridge
-    }
-  })
-}
-
 async function deployErcToNative() {
   const preDeploy = require('./src/erc_to_native/preDeploy')
   const deployHome = require('./src/erc_to_native/home')
@@ -125,150 +80,9 @@ async function deployAMBErcToErc() {
   })
 }
 
-async function deployStakeAMBErcToErc() {
-  const preDeploy = require('./src/stake_token_mediators/preDeploy')
-  const deployHome = require('./src/stake_token_mediators/home')
-  const deployForeign = require('./src/stake_token_mediators/foreign')
-  const initialize = require('./src/stake_token_mediators/initialize')
-  await preDeploy()
-  const { homeBridgeMediator } = await deployHome()
-  const { foreignBridgeMediator } = await deployForeign()
-  await initialize({ homeBridge: homeBridgeMediator.address, foreignBridge: foreignBridgeMediator.address })
-  console.log('\nDeployment has been completed.\n\n')
-  console.log(`[   Home  ] Bridge Mediator: ${homeBridgeMediator.address}`)
-  console.log(`[ Foreign ] Bridge Mediator: ${foreignBridgeMediator.address}`)
-  writeDeploymentResults({
-    homeBridge: {
-      homeBridgeMediator
-    },
-    foreignBridge: {
-      foreignBridgeMediator
-    }
-  })
-}
-
-async function deployAMBNativeToErc() {
-  const preDeploy = require('./src/amb_native_to_erc20/preDeploy')
-  const deployHome = require('./src/amb_native_to_erc20/home')
-  const deployForeign = require('./src/amb_native_to_erc20/foreign')
-  const initializeHome = require('./src/amb_native_to_erc20/initializeHome')
-  const initializeForeign = require('./src/amb_native_to_erc20/initializeForeign')
-  await preDeploy()
-  const { homeBridgeMediator, homeFeeManager } = await deployHome()
-  const { foreignBridgeMediator, bridgeableErc677, foreignFeeManager } = await deployForeign()
-
-  await initializeHome({
-    homeBridge: homeBridgeMediator.address,
-    homeFeeManager: homeFeeManager.address,
-    foreignBridge: foreignBridgeMediator.address
-  })
-
-  await initializeForeign({
-    foreignBridge: foreignBridgeMediator.address,
-    foreignFeeManager: foreignFeeManager.address,
-    foreignErc677: bridgeableErc677.address,
-    homeBridge: homeBridgeMediator.address
-  })
-
-  console.log('\nDeployment has been completed.\n\n')
-  console.log(`[   Home  ] Bridge Mediator: ${homeBridgeMediator.address}`)
-  if (homeFeeManager.address) {
-    console.log(`[   Home  ] Fee Manager: ${homeFeeManager.address}`)
-  }
-  console.log(`[ Foreign ] Bridge Mediator: ${foreignBridgeMediator.address}`)
-  if (foreignFeeManager.address) {
-    console.log(`[ Foreign ] Fee Manager: ${foreignFeeManager.address}`)
-  }
-  console.log(`[ Foreign ] ERC677 Token: ${bridgeableErc677.address}`)
-  writeDeploymentResults({
-    homeBridge: {
-      homeBridgeMediator,
-      homeFeeManager
-    },
-    foreignBridge: {
-      foreignBridgeMediator,
-      foreignFeeManager,
-      bridgeableErc677
-    }
-  })
-}
-
-async function deployAMBErcToNative() {
-  const preDeploy = require('./src/amb_erc20_to_native/preDeploy')
-  const deployHome = require('./src/amb_erc20_to_native/home')
-  const deployForeign = require('./src/amb_erc20_to_native/foreign')
-  const initializeHome = require('./src/amb_erc20_to_native/initializeHome')
-  const initializeForeign = require('./src/amb_erc20_to_native/initializeForeign')
-  await preDeploy()
-  const { homeBridgeMediator } = await deployHome()
-  const { foreignBridgeMediator } = await deployForeign()
-
-  await initializeHome({
-    homeBridge: homeBridgeMediator.address,
-    foreignBridge: foreignBridgeMediator.address
-  })
-
-  await initializeForeign({
-    foreignBridge: foreignBridgeMediator.address,
-    homeBridge: homeBridgeMediator.address
-  })
-
-  console.log('\nDeployment has been completed.\n\n')
-  console.log(`[   Home  ] Bridge Mediator: ${homeBridgeMediator.address}`)
-  console.log(`[ Foreign ] Bridge Mediator: ${foreignBridgeMediator.address}`)
-  writeDeploymentResults({
-    homeBridge: {
-      homeBridgeMediator
-    },
-    foreignBridge: {
-      foreignBridgeMediator
-    }
-  })
-}
-
-async function deployMultiAMBErcToErc() {
-  const preDeploy = require('./src/multi_amb_erc20_to_erc677/preDeploy')
-  const deployHome = require('./src/multi_amb_erc20_to_erc677/home')
-  const deployForeign = require('./src/multi_amb_erc20_to_erc677/foreign')
-  const initializeHome = require('./src/multi_amb_erc20_to_erc677/initializeHome')
-  const initializeForeign = require('./src/multi_amb_erc20_to_erc677/initializeForeign')
-  await preDeploy()
-  const { homeBridgeMediator, homeTokenImage } = await deployHome()
-  const { foreignBridgeMediator } = await deployForeign()
-
-  await initializeHome({
-    homeBridge: homeBridgeMediator.address,
-    foreignBridge: foreignBridgeMediator.address,
-    homeTokenImage: homeTokenImage.address
-  })
-
-  await initializeForeign({
-    foreignBridge: foreignBridgeMediator.address,
-    homeBridge: homeBridgeMediator.address
-  })
-
-  console.log('\nDeployment has been completed.\n\n')
-  console.log(`[   Home  ] Bridge Mediator: ${homeBridgeMediator.address}`)
-  console.log(`[ Foreign ] Bridge Mediator: ${foreignBridgeMediator.address}`)
-  writeDeploymentResults({
-    homeBridge: {
-      homeBridgeMediator
-    },
-    foreignBridge: {
-      foreignBridgeMediator
-    }
-  })
-}
-
 async function main() {
   console.log(`Bridge mode: ${BRIDGE_MODE}`)
   switch (BRIDGE_MODE) {
-    case 'NATIVE_TO_ERC':
-      await deployNativeToErc()
-      break
-    case 'ERC_TO_ERC':
-      await deployErcToErc()
-      break
     case 'ERC_TO_NATIVE':
       await deployErcToNative()
       break
@@ -278,21 +92,9 @@ async function main() {
     case 'AMB_ERC_TO_ERC':
       await deployAMBErcToErc()
       break
-    case 'STAKE_AMB_ERC_TO_ERC':
-      await deployStakeAMBErcToErc()
-      break
-    case 'AMB_NATIVE_TO_ERC':
-      await deployAMBNativeToErc()
-      break
-    case 'AMB_ERC_TO_NATIVE':
-      await deployAMBErcToNative()
-      break
-    case 'MULTI_AMB_ERC_TO_ERC':
-      await deployMultiAMBErcToErc()
-      break
     default:
       console.log(BRIDGE_MODE)
-      throw new Error('Please specify BRIDGE_MODE: NATIVE_TO_ERC or ERC_TO_ERC')
+      throw new Error('Please specify BRIDGE_MODE: ERC_TO_NATIVE or ARBITRARY_MESSAGE or AMB_ERC_TO_ERC')
   }
 }
 
--- flatten.sh
+++ flatten.sh
@@ -4,16 +4,11 @@ if [ -d flats ]; then
   rm -rf flats
 fi
 
-mkdir -p flats/native_to_erc20
-mkdir -p flats/erc20_to_erc20
 mkdir -p flats/erc20_to_native
 mkdir -p flats/validators
 mkdir -p flats/arbitrary_message
 mkdir -p flats/amb_erc677_to_erc677
 mkdir -p flats/upgradeability
-mkdir -p flats/amb_native_to_erc20
-mkdir -p flats/amb_erc20_to_native
-mkdir -p flats/multi_amb_erc20_to_erc677
 
 FLATTENER=./node_modules/.bin/truffle-flattener
 BRIDGE_CONTRACTS_DIR=contracts/upgradeable_contracts
@@ -29,25 +24,11 @@ echo "Flattening bridge validators contracts"
 ${FLATTENER} ${VALIDATOR_CONTRACTS_DIR}/BridgeValidators.sol > flats/validators/BridgeValidators_flat.sol
 ${FLATTENER} ${VALIDATOR_CONTRACTS_DIR}/RewardableValidators.sol > flats/validators/RewardableValidators_flat.sol
 
-echo "Flattening contracts related to native-to-erc bridge"
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/native_to_erc20/ForeignBridgeNativeToErc.sol > flats/native_to_erc20/ForeignBridgeNativeToErc_flat.sol
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/native_to_erc20/HomeBridgeNativeToErc.sol > flats/native_to_erc20/HomeBridgeNativeToErc_flat.sol
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/native_to_erc20/FeeManagerNativeToErc.sol > flats/native_to_erc20/FeeManagerNativeToErc_flat.sol
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/native_to_erc20/FeeManagerNativeToErcBothDirections.sol > flats/native_to_erc20/FeeManagerNativeToErcBothDirections_flat.sol
-
-echo "Flattening contracts related to erc-to-erc bridge"
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/erc20_to_erc20/HomeBridgeErcToErc.sol > flats/erc20_to_erc20/HomeBridgeErcToErc_flat.sol
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/erc20_to_erc20/HomeBridgeErcToErcPOSDAO.sol > flats/erc20_to_erc20/HomeBridgeErcToErcPOSDAO_flat.sol
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/erc20_to_erc20/ForeignBridgeErcToErc.sol > flats/erc20_to_erc20/ForeignBridgeErcToErc_flat.sol
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/erc20_to_erc20/ForeignBridgeErc677ToErc677.sol > flats/erc20_to_erc20/ForeignBridgeErc677ToErc677_flat.sol
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/erc20_to_erc20/FeeManagerErcToErcPOSDAO.sol > flats/erc20_to_erc20/FeeManagerErcToErcPOSDAO_flat.sol
-
 echo "Flattening contracts related to erc-to-native bridge"
 ${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/erc20_to_native/HomeBridgeErcToNative.sol > flats/erc20_to_native/HomeBridgeErcToNative_flat.sol
 ${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/erc20_to_native/ForeignBridgeErcToNative.sol > flats/erc20_to_native/ForeignBridgeErcToNative_flat.sol
 ${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/erc20_to_native/FeeManagerErcToNative.sol > flats/erc20_to_native/FeeManagerErcToNative_flat.sol
 ${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/erc20_to_native/FeeManagerErcToNativePOSDAO.sol > flats/erc20_to_native/FeeManagerErcToNativePOSDAO_flat.sol
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/InterestReceiver.sol > flats/InterestReceiver_flat.sol
 
 echo "Flattening contracts related to arbitrary-message bridge"
 ${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/arbitrary_message/HomeAMB.sol > flats/arbitrary_message/HomeAMB_flat.sol
@@ -57,21 +38,3 @@ ${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/arbitrary_message/ForeignAMBWithGasToken.so
 echo "Flattening contracts related to erc677 to erc677 on top of AMB bridge"
 ${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/amb_erc677_to_erc677/HomeAMBErc677ToErc677.sol > flats/amb_erc677_to_erc677/HomeAMBErc677ToErc677_flat.sol
 ${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/amb_erc677_to_erc677/ForeignAMBErc677ToErc677.sol > flats/amb_erc677_to_erc677/ForeignAMBErc677ToErc677_flat.sol
-
-echo "Flattening contracts related to stake token mediators"
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/amb_erc677_to_erc677/HomeStakeTokenMediator.sol > flats/amb_erc677_to_erc677/HomeStakeTokenMediator_flat.sol
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/amb_erc677_to_erc677/ForeignStakeTokenMediator.sol > flats/amb_erc677_to_erc677/ForeignStakeTokenMediator_flat.sol
-
-echo "Flattening contracts related to native-to-erc on top of AMB bridge"
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/amb_native_to_erc20/HomeAMBNativeToErc20.sol > flats/amb_native_to_erc20/HomeAMBNativeToErc20_flat.sol
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/amb_native_to_erc20/ForeignAMBNativeToErc20.sol > flats/amb_native_to_erc20/ForeignAMBNativeToErc20_flat.sol
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/amb_native_to_erc20/HomeFeeManagerAMBNativeToErc20.sol > flats/amb_native_to_erc20/HomeFeeManagerAMBNativeToErc20_flat.sol
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/amb_native_to_erc20/ForeignFeeManagerAMBNativeToErc20.sol > flats/amb_native_to_erc20/ForeignFeeManagerAMBNativeToErc20_flat.sol
-
-echo "Flattening contracts related to erc-to-native on top of AMB bridge"
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/amb_erc20_to_native/HomeAMBErc20ToNative.sol > flats/amb_erc20_to_native/HomeAMBErc20ToNative_flat.sol
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/amb_erc20_to_native/ForeignAMBErc20ToNative.sol > flats/amb_erc20_to_native/ForeignAMBErc20ToNative_flat.sol
-
-echo "Flattening contracts related to multi-erc-to-erc on top of AMB bridge"
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/multi_amb_erc20_to_erc677/HomeMultiAMBErc20ToErc677.sol > flats/multi_amb_erc20_to_erc677/HomeMultiAMBErc20ToErc677_flat.sol
-${FLATTENER} ${BRIDGE_CONTRACTS_DIR}/multi_amb_erc20_to_erc677/ForeignMultiAMBErc20ToErc677.sol > flats/multi_amb_erc20_to_erc677/ForeignMultiAMBErc20ToErc677_flat.sol

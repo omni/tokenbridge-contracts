f862da73f30b7b126efa49028fb360e1e3f25b0c
Kirill Fedoseev <k.fedoseev@innopolis.university>
2021-08-04 00:33:58 +0400

    Make _sendMessage internal (#613)

--- contracts/upgradeable_contracts/arbitrary_message/MessageDelivery.sol
+++ contracts/upgradeable_contracts/arbitrary_message/MessageDelivery.sol
@@ -28,7 +28,7 @@ contract MessageDelivery is BasicAMB, MessageProcessor {
     * @param _gas gas limit used on the other network for executing a message
     * @param _dataType AMB message dataType to be included as a part of the header
     */
-    function _sendMessage(address _contract, bytes _data, uint256 _gas, uint256 _dataType) public returns (bytes32) {
+    function _sendMessage(address _contract, bytes _data, uint256 _gas, uint256 _dataType) internal returns (bytes32) {
         // it is not allowed to pass messages while other messages are processed
         // if other is not explicitly configured
         require(messageId() == bytes32(0) || allowReentrantRequests());

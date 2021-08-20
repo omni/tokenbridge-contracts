39f5835198f3d1728659de9dd959e7a2ae53f7ca
Kirill Fedoseev <k.fedoseev@innopolis.university>
2021-08-20 14:14:21 +0300

    Bump package and contracts interfaces version prior to 6.0.0 (#629)

--- contracts/upgradeable_contracts/VersionableBridge.sol
+++ contracts/upgradeable_contracts/VersionableBridge.sol
@@ -2,7 +2,7 @@ pragma solidity 0.4.24;
 
 contract VersionableBridge {
     function getBridgeInterfacesVersion() external pure returns (uint64 major, uint64 minor, uint64 patch) {
-        return (6, 0, 0);
+        return (6, 1, 0);
     }
 
     /* solcov ignore next */
